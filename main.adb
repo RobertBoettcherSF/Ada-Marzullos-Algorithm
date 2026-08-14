d7172bc4d822f0695e5d1e2812401be19022d7ef
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-14 13:23:58 +0200

    Implement main procedure with output messages

---
+++ main.adb
@@ -0,0 +1,7 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Main is
+begin
+   Put_Line ("Marzullo's Algorithm compiled successfully.");
+   Put_Line ("To verify system integrity, run: make test");
+end Main;
